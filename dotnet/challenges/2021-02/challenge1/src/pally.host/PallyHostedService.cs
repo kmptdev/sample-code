f663a96e12f515a2045a5022eb95fe9161942abb
Ross Buggins <78215796+RossBugginsNHS@users.noreply.github.com>
2021-02-19 21:04:30 +0000

    Added pattern matching.

--- dotnet/challenges/2021-02/challenge1/src/pally.host/PallyHostedService.cs
+++ dotnet/challenges/2021-02/challenge1/src/pally.host/PallyHostedService.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Logging;
 
 namespace challenge1
 {
+
+
     public class PallyHostedService : IHostedService
     {
         private readonly ILogger<PallyHostedService> _logger;
@@ -17,27 +19,66 @@ namespace challenge1
             _logger = logger;
         }
 
+        private bool ContainsTrue(string fileName) => new FileInfo(fileName).Name.ToLower().Contains("true");
+        private bool ContainsFalse(string fileName) => new FileInfo(fileName).Name.ToLower().Contains("false");
+
+
+        private bool Correct(PallyResult pallyResult) =>
+        (ContainsTrue(pallyResult.fileName), ContainsFalse(pallyResult.fileName), pallyResult.result) switch
+        {
+            (true, _, true) => true,
+            (_, true, false) => true,
+            _ => false
+        };
+
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            var fails = new List<PallyResult>();
-            await foreach(var result in _testEngine.TestAsync())
+            await TestAllLogFailed(cancellationToken);
+        }
+
+        private async Task TestAllLogFailed(CancellationToken cancellationToken)
+        {
+            var failedResults = await TestAllSeparateFailed(cancellationToken);
+            ErrorOutput(failedResults);
+        }
+
+        private async Task<IEnumerable<PallyResult>> TestAllSeparateFailed( CancellationToken cancellationToken)
+        {
+            List<PallyResult> failedResults = new List<PallyResult>();
+            await foreach (var result in TestAll(cancellationToken))
             {
-                _logger.LogInformation("Result {result}", result);
-                var fileInfo = new FileInfo(result.fileName);
-                var fileName = fileInfo.Name.ToLower();
-                if(fileName.Contains("true") && !result.result)
-                {
-                    fails.Add(result);
-                }
-                else if(fileName.Contains("false") && result.result)
-                {
-                    fails.Add(result);
-                }
+                AddToListIfFailed(failedResults, result);
             }
 
-            foreach(var fail in fails)
+            return failedResults;
+        }
+
+        private void AddToListIfFailed(List<PallyResult> failedResults, PallyResult result)
+        {
+            if (!Correct(result))
+                    failedResults.Add(result);
+        }
+
+        private async IAsyncEnumerable<PallyResult> TestAll(CancellationToken cancellationToken)
+        {
+            await foreach (var result in _testEngine.TestAsync())
             {
-                 _logger.LogError("Failed Result {result}", fail);
+                await Test(result);
+                yield return result;
+            }
+        }
+
+        private Task Test(PallyResult result)
+        {
+            _logger.LogInformation("Result {result}", result);
+            return Task.CompletedTask;
+        }
+
+        private void ErrorOutput(IEnumerable<PallyResult> failedResults)
+        {
+            foreach (var fail in failedResults)
+            {
+                _logger.LogError("Failed Result {result}", fail);
             }
         }
 
